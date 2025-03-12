4557c2925cee78ad3e19ea88a597aada6cb1da1b
RebecaSLChagas <rebecaslchagas@gmail.com>
2025-03-12 20:23:33 -0300

    primeira-aula

--- src/MainProduto.java
+++ src/MainProduto.java
@@ -2,6 +2,17 @@ import exe3.Produto;
 
 public class MainProduto {
     public static void main(String args[]){
+        //chama construtor sem parâmetro
+        Produto obj1 = new Produto();
+        //chama consrtutor com parâmetro
+        Produto obj2 = new Produto(123,5,"Bola de Basquete",500);
+        System.out.println(obj1.exibeDetalhes());
+        System.out.println(obj2.exibeDetalhes());
 
+        obj2.comprar(3);
+        obj2.subir(50);
+        obj2.vender(1);
+        obj2.descer(560);
+        System.out.println(obj2.exibeDetalhes());
     }
 }
--- src/exe3/Produto.java
+++ src/exe3/Produto.java
@@ -2,12 +2,55 @@ package exe3;
 
 public class Produto {
     public int id, qtde;
-
     public String descricao;
-
     public float preco;
+
+    //criar um objeto de produto em valor
+    public Produto(){
+        this.descricao = "sem descrição"; //null
+    }
+    public Produto(int id, int qtde, String descricao, float preco){
+        this.id = id;
+        this.qtde = qtde;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    //comprar x unidades
+    public void comprar(int x){
+        this.qtde += x;
+    }
+    //subir preço em x unidades
+    public void subir(float x){
+        this.preco += x;
+    }
+    //vender x unidades
+    public void vender(int x){
+        if(this.qtde >= x){
+            this.qtde -=x;
+        }
+        else{
+            System.out.println("Estoque esgotado");
+        }
+    }
+    //descer x unidades
+    public void descer(float x){
+        if(this.preco >= x){
+            this.preco -= x;
+        }
+        else{
+            System.out.println("Preço não pode ser negativo");
+        }
+    }
+
+    public String exibeDetalhes() { //clicar no botão direito, generate, ok, colocar this necessários)
+        return "Produto{" +
+                "id=" + this.id +
+                ", qtde=" + this.qtde +
+                ", descricao='" + this.descricao + '\'' +
+                ", preco=" + this.preco +
+                '}';
+    }
 }
 
-public Produto(){
-    this.descricao = "descrição";
-}
+
